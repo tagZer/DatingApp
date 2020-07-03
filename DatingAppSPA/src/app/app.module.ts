78c86d0eabed44a7c0bb0199ff20394308f8afcf
Vojislav Vukovic <v.vukovic@live.com>
2020-07-03 23:45:31 +0200

    Various changes regarding frontend: - Auth0 sends token automatically now, - added users lists and user detailed page - added photo gallery - added route resolvers

--- DatingAppSPA/src/app/app.module.ts
+++ DatingAppSPA/src/app/app.module.ts
@@ -4,23 +4,37 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 // components
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberCardComponent } from './members/member-list/member-card/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
 
 // services
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
+// resolvers
+import { MemberDetailResolver } from './_resolvers/member-detailed.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+
 // ngx
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { appRoutes } from './routes';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
+// auth
+export function tokenGetter() {
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -29,8 +43,10 @@ import { appRoutes } from './routes';
       RegisterComponent,
       HomeComponent,
       MemberListComponent,
+      MemberCardComponent,
       ListsComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberDetailComponent
    ],
    imports: [
       BrowserModule,
@@ -38,11 +54,22 @@ import { appRoutes } from './routes';
       FormsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
+      TabsModule.forRoot(),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter,
+            whitelistedDomains: ['localhost:5000'],
+            blacklistedRoutes: ['localhost:5000/api/auth']
+         }
+      })
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
---
+++ DatingAppSPA/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../_models/user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-list',
+  templateUrl: './member-list.component.html',
+  styleUrls: ['./member-list.component.css'],
+})
+export class MemberListComponent implements OnInit {
+  users: User[];
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    const users = 'users';
+
+    this.route.data.subscribe((data) => {
+      this.users = data[users];
+    });
+  }
+}
